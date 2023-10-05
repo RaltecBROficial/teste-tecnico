a5b8ccb4d7a65627335437acfcc879af2cf20ca1
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2023-10-05 18:03:15 -0300

    Add profiles (automapper) for entities and view models (Product and Sale)

---
+++ algoritmos/TesteTecnico.Raltec01/TesteTecnico.Raltec01.Application/AutoMapper/SaleProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TesteTecnico.Raltec01.Application.ViewModels;
+using TesteTecnico.Raltec01.Domain.Entities;
+
+namespace TesteTecnico.Raltec01.Application.AutoMapper
+{
+	public class SaleProfile : Profile
+	{
+		public SaleProfile()
+		{
+			CreateMap<Sale, SaleViewModel>()
+				.ForSourceMember(sale => sale.Items, options => options.DoNotValidate())
+				.ReverseMap()
+				.ForMember(sale => sale.Items, options => options.Ignore());
+		}
+	}
+}
