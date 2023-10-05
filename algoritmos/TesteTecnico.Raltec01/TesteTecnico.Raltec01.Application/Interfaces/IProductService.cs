d6005c60210341ee73fb8263f1d2270d63827e7f
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2023-10-05 16:20:19 -0300

    Create product service interface

---
+++ algoritmos/TesteTecnico.Raltec01/TesteTecnico.Raltec01.Application/Interfaces/IProductService.cs
@@ -0,0 +1,8 @@
+﻿using TesteTecnico.Raltec01.Application.ViewModels;
+
+namespace TesteTecnico.Raltec01.Application.Interfaces
+{
+	public interface IProductService : IAppService<ProductViewModel>
+	{
+	}
+}
