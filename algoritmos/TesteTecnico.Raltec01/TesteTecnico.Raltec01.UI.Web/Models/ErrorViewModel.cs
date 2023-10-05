510754baba88bb308dd1442c50f52eacd1b72ed2
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2023-10-05 15:59:16 -0300

    Create default asp.net core mvc project

---
+++ algoritmos/TesteTecnico.Raltec01/TesteTecnico.Raltec01.UI.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TesteTecnico.Raltec01.UI.Web.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
