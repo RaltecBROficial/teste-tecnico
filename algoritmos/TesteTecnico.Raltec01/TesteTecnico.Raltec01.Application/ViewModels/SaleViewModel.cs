bcd1741469bbf9f20cc2544391d6227147df501a
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2023-10-05 16:16:25 -0300

    Add view model for sale

---
+++ algoritmos/TesteTecnico.Raltec01/TesteTecnico.Raltec01.Application/ViewModels/SaleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TesteTecnico.Raltec01.Application.ViewModels
+{
+	public class SaleViewModel
+	{
+		public Guid Id { get; set; }
+		public int CodeSale { get; set; }
+		public DateTime DateAndHour { get; set; }
+	}
+}
