d9532e563c81a0ac48a06d5f429f97d6e238e4eb
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2023-10-05 16:16:48 -0300

    Create base service for application

---
+++ algoritmos/TesteTecnico.Raltec01/TesteTecnico.Raltec01.Application/Interfaces/IAppService.cs
@@ -0,0 +1,11 @@
+﻿namespace TesteTecnico.Raltec01.Application.Interfaces
+{
+	public interface IAppService<T> : IAsyncDisposable where T : class
+	{
+		Task<T> GetByIdAsync(Guid id);
+		Task<IEnumerable<T>> GetAllAsync();
+		Task AddAsync(T entity);
+		Task UpdateAsync(T entity);
+		Task RemoveAsync(T entity);
+	}
+}
