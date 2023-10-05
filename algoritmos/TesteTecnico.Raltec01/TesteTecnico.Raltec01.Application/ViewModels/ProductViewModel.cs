6e91f03a46df8330faf8369abfc6c461c6c4bd34
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2023-10-05 16:20:03 -0300

    Add view model for product

---
+++ algoritmos/TesteTecnico.Raltec01/TesteTecnico.Raltec01.Application/ViewModels/ProductViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TesteTecnico.Raltec01.Application.ViewModels
+{
+	public class ProductViewModel
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public decimal UnitValue { get; set; }
+	}
+}
