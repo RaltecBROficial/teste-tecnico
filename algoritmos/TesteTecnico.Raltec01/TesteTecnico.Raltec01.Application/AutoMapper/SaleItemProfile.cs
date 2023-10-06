dc4546df703fbbac8cee9074c832c5385127ecd0
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2023-10-06 18:23:57 -0300

    Add SaleItemProfile

---
+++ algoritmos/TesteTecnico.Raltec01/TesteTecnico.Raltec01.Application/AutoMapper/SaleItemProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using TesteTecnico.Raltec01.Application.ViewModels;
+using TesteTecnico.Raltec01.Domain.Entities;
+
+namespace TesteTecnico.Raltec01.Application.AutoMapper
+{
+	public class SaleItemProfile : Profile
+	{
+		public SaleItemProfile()
+		{
+			CreateMap<SaleItem, SaleItemViewModel>()
+				.ForSourceMember(saleItem => saleItem.Product, options => options.DoNotValidate())
+				.ForSourceMember(saleItem => saleItem.Sale, options => options.DoNotValidate())
+				.ReverseMap()
+				.ForMember(saleViewModel => saleViewModel.Product, options => options.Ignore())
+				.ForMember(saleViewModel => saleViewModel.Sale, options => options.Ignore());
+		}
+	}
+}
